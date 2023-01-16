afc4c763c3eba4839b3112ee55e73aeaffaa6e1c
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-01-15 16:08:05 -0800

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,5 +1,9 @@
+#
+# test.asm - an example of enhanced RiSC-16 assembly
+#
+
 RAM_END EQU 0x3ff           # 1K of RAM
-SSEG EQU 0x400
+SSEG EQU 0x400              # MMIO address for SSEG display
 
 start:  
         movi sp, RAM_END    # setup SP to point to top of RAM
@@ -7,13 +11,11 @@ start:
         add r1, r0, r0      # zero R1
         movi r2, disp       # get address of disp routine
 
-loop:   inc r1
-        #addi r1, r1, 1      # inc counter
-        call r2
-        #jalr lr, r2         # call subroutine
+loop:   inc r1              # increment counter in R1
+        call r2             # call subroutine to update display
         beq r0, r0, loop    # do it again
 
 disp:   sw r1, r5, 0        # output to SSEG
-        ret
-        #jalr r0, lr         # return
+        ret                 # return
+
         
